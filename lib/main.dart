807a2849830fe43478c8c9af343cf66cd81f11dd
Nicole <105656415+nicole-yc@users.noreply.github.com>
2026-08-27 11:50:46 -0700

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'app/cadence_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'app/cadence_app.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
